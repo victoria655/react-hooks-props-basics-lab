4f42e401c95188756c45533b9cc4da04ee3b2a27
victoria655 <karonjivictoria@gmail.com>
2025-04-10 18:40:34 +0300

    modified  app.js,about.js,home.js,links.js,Navbar.js and added links.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,8 @@
 import React from "react";
 
+
 function Home(props) {
+  
   return (
     <div id="home">
       <h1 style={{ color: props.color }}>
---
+++ src/components/Links.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+// src/components/Links.js
+
+function Links({ github, linkedin }) {
+  return (
+    <div>
+      <h3>Links</h3>
+      <a href={github}>{github}</a>
+      <a href={linkedin}>{linkedin}</a>
+    </div>
+  );
+}
+
+
+
+export default Links
+
